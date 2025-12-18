b07dca03b49d6ab280c0b17841dad420eb0ce562
Ayush shahi <ayush.shahi.147@gmail.com>
2025-12-18 21:33:59 +0545

    choose mode page

---
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -0,0 +1,105 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/assets_vectors.dart';
+
+class ChooseMode extends StatelessWidget {
+  const ChooseMode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.chooseModeBG),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.15)),
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                const Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xff30393C).withOpacity(0.5),
+                            ),
+                            child: SvgPicture.asset(
+                              AppVectors.moon,
+                              fit: BoxFit.none,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 17),
+                        Text(
+                          'Dark Mode',
+                          style: TextStyle(color: Colors.white, fontSize: 17),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 40),
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xff30393C).withOpacity(0.5),
+                            ),
+                            child: SvgPicture.asset(
+                              AppVectors.sun,
+                              fit: BoxFit.none,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 17),
+                        Text(
+                          'Light Mode',
+                          style: TextStyle(color: Colors.white, fontSize: 17),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+                BasicAppButton(onPressed: () {}, title: 'Choose'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/assets_vectors.dart';
+import 'package:spotify/presentation/choose_mode/pages/choose_mode.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBG),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.15)),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  "Enjoy Listening To Music",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 21),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const ChooseMode(),
+                      ),
+                    );
+                  },
+                  title: "Get Started",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
