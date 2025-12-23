e6d862bc2a3d7ba9af5bdc644fa50d14c4516c18
Ayush shahi <ayush.shahi.147@gmail.com>
2025-12-23 21:55:16 +0545

    worked on datalayer but extending the method from another class is remaining

---
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -0,0 +1,12 @@
+import 'package:spotify/domain/repository/auth/auth.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<void> signin() {
+    // TODO: implement signin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup() {}
+}
---
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify/data/model/auth/create_user_req.dart';
+
+abstract class AutFirebaseService {
+  Future<void> signup(CreateUserReq createUserReq);
+
+  Future<void> signin();
+}
+
+class AuthFirebaseServiceImpl extends AutFirebaseService {
+  @override
+  Future<void> signin() {
+    // TODO: implement signin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup(CreateUserReq createUserReq) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: createUserReq.email,
+        password: createUserReq.password,
+      );
+    } on FirebaseAuthException {}
+  }
+}
